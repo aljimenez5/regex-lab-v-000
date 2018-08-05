9ad4765073be2056045bf08d98ffb7518696d4a3
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 04:52:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  until phone.scan(/[^A-Z]/i) == true
+  until phone.scan(/[A-Z]/i) == true
     return true
   end
 end
