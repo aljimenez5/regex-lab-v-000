8869a3a62dfe92d1f8e30f037dd128fe18a60a30
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 04:53:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/[A-Z]/i) == true
+  if phone.scan(/[a-z]/i) == true
     return false
   else
     return true
